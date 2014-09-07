01a3f03a5a63f10f2a25ca71880f391d93672169
Nik Cubrilovic <nikcub@gmail.com>
2014-09-08 09:33:18 +1000

    add recent posts

--- single.php
+++ single.php
@@ -7,6 +7,17 @@
 
         <?php get_template_part( 'content', get_post_format() ); ?>
 
+        <h2>Recent Posts</h2>
+        <ul>
+<?php
+  $args = array( 'numberposts' => '5' );
+  $recent_posts = wp_get_recent_posts( $args );
+  foreach( $recent_posts as $recent ){
+    echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+  }
+?>
+</ul>
+
         <?php comments_template( '', true ); ?>
 
       <?php endwhile; // end of the loop. ?>
